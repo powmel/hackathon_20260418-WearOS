7ce038954a8dd1b727d2c97d7a5a1ea390c16a24
Powmel <taiki.msw@gmail.com>
2026-04-18 17:21:14 +0900

    Add Wear OS rhino pet companion app

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "hackathon20260418-wear"
+include(":app")
